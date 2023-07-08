7206f396c07e904094cc5eae0fb831b7f93630e1
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-08 12:56:12 +0530

    created basic html structure for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,17 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div>
+        <div className='navbar_title_container'>
+            <p className='navbar_title'>ELINEKART</p>
+        </div>
+        <div className='navbar_search_container'>
 
+        </div>
+        <div className='navbar_links_container'>
+            <a className='navbar_links' href='#'>CART</a>
+            <a className='navbar_links' href='#'>SIGN IN</a>
+            <a className='navbar_links' href='#'>REGISTER</a>
+        </div>
     </div>
   )
 }
