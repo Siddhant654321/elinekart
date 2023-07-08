2e791960531669cbd7476f8c1957dcbb64efe7f9
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-08 13:41:34 +0530

    created the search field

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import './Navbar.css'
+import './Navbar.css';
+import search_icon from '../assets/search_icon.svg'; 
+
 const Navbar = () => {
   return (
     <div className='navbar_container'>
         <div className='navbar_title_container'>
             <p className='navbar_title'>ELINEKART</p>
         </div>
-        <div className='navbar_search_container'>
-
-        </div>
+        <form className='navbar_search_container'>
+            <input type='text' placeholder='Search Products' className='navbar_search_input' />
+            <button type='submit' className='navbar_search_button'><img src={search_icon} height={34} width={34} /></button>
+        </form>
         <div className='navbar_links_container'>
             <a className='navbar_links' href='#'>CART</a>
             <a className='navbar_links' href='#'>SIGN IN</a>
