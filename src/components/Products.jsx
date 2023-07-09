4b53d7c408611b2cd6046c8ae5e420a887c153ac
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-09 12:45:52 +0530

    styled the parent div of the products list where ProductTab will be rendered later in the development

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,6 +4,9 @@ const Products = () => {
   return (
     <section className='products_section'>
         <h1 className="products_heading">OUR PRODUCTS</h1>
+        <div className='products_list'>
+        
+        </div>
     </section>
   )
 }
