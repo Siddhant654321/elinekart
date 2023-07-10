09f9608290307433dc8e5c2a38295e3437fb9b99
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-10 14:11:30 +0530

    created the updateUser controller for the admin to update the profile a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -154,6 +154,31 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Update user
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 export {
   authUser,
   registerUser,
@@ -162,5 +187,6 @@ export {
   updateUserProfile,
   getUsers,
   getUserByID,
-  deleteUser
+  deleteUser,
+  updateUser
 };
