c14871f3e867d78870a13cf7c6494110c99a523d
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-10 13:12:33 +0530

    created the getUserProfile controller to get the user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,6 +67,25 @@ const logoutUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Logged out successfully!" });
 });
 
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 export {
   authUser,
   registerUser,
