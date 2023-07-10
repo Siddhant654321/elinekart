4974c359906ec7d5152b843e792f6dde17294d37
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-10 12:54:51 +0530

    created the controller file for the user collection and created the controller to login the user

---
+++ backend/controllers/userController.js
@@ -0,0 +1,31 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+
+// @desc Auth user & get token
+// @route POST /api/users/login
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+
+export {
+  authUser,
+};
