06b7a67a3efeec7e121fc9255aeacc724207462e
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-10 12:54:51 +0530

    created the registerUser controller to create a new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,7 +25,37 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Register user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 export {
   authUser,
+  registerUser
 };
