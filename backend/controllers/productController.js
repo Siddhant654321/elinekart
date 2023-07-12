e0efb98dbf08d2a7339ee67f8d23692e11390c8f
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-12 10:47:17 +0530

    created the controller to fetch a product using its id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,6 +25,21 @@ const getProducts = asyncHandler(async (req, res) => {
   res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
+// @desc Fetch a product
+// @route GET /api/products/:id
+// @access Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
 export {
   getProducts,
+  getProductById
 };
