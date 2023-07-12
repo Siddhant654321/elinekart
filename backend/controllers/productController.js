d4cb70478a59221ba5800e8f385b9021bd92d1ca
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-12 11:11:28 +0530

    created the controller to update a product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -58,8 +58,35 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(createdProduct);
 });
 
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
 export {
   getProducts,
   getProductById,
-  createProduct
+  createProduct,
+  updateProduct
 };
