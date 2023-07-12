1f67ad883fbfc094d32e344d3d48614b73e3eab2
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-12 12:10:34 +0530

    added the product routes to the server.js file

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 
 const port = process.env.PORT || 3500;
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
+app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
