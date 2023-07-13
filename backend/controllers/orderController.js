406c96ba8aa6c8c1939617c9ca971b5678fdd141
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-13 11:10:24 +0530

    created the orderController.js file and the controller to create a new order

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,48 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Order from "../models/orderModel.js";
+
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    // Create new order
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    // Save order to database
+    const createdOrder = await order.save();
+
+    // Send response
+    res.status(201).json(createdOrder);
+  }
+});
+
+export {
+  addOrderItems,
+};
