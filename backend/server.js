fec56bab6d09f5ec2a40c4f73a5328868419530e
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-13 13:58:13 +0530

    added paypal payment support

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,9 @@ app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID }),
+);
 
 app.use(notFound);
 app.use(errorHandler);
