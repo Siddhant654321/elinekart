d8ccae9c2de81ee402f54a71fccfa88f5ca69a08
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-13 11:33:09 +0530

    created the controller to get order by id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -51,7 +51,25 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.status(200).json(orders);
 });
 
+// @desc    Get order by ID orders
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email",
+  );
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
 export {
   addOrderItems,
   getMyOrders,
+  getOrderById,
 };
