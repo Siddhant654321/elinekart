06dc50292bc7cad895c31277c2be7f4cb3407fb1
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-13 12:47:49 +0530

    added order routes file to server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 
 const port = process.env.PORT || 3500;
@@ -21,5 +22,6 @@ app.use(cookieParser());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
