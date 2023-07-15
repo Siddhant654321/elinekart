a93efcc39da66846edd16cf727494a663caabb95
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-15 13:07:16 +0530

    created contants.js file to hold url of different endpoints

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "http://localhost:3000";
+export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/users";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
+
+export const UPLOAD_URL = "/api/upload";
