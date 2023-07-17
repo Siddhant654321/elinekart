aefeca4fb237307d975a8d612cf7badb37f11a28
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-17 10:31:35 +0530

    changed sample data and added the product images for the sample data

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,76 +1,608 @@
-const products = [
-  {
-    name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
-    description:
-      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
-    brand: "Apple",
-    category: "Electronics",
-    price: 89.99,
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 12,
+const products = [{
+  "_id": {
+    "$oid": "651526c28f374b07b16c901b"
   },
-  {
-    name: "iPhone 13 Pro 256GB Memory",
-    image: "/images/phone.jpg",
-    description:
-      "Introducing the iPhone 13 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
-    brand: "Apple",
-    category: "Electronics",
-    price: 599.99,
-    countInStock: 7,
-    rating: 4.0,
-    numReviews: 8,
+  "user": {
+    "$oid": "651526c28f374b07b16c9003"
   },
-  {
-    name: "Cannon EOS 80D DSLR Camera",
-    image: "/images/camera.jpg",
-    description:
-      "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
-    brand: "Cannon",
-    category: "Electronics",
-    price: 929.99,
-    countInStock: 5,
-    rating: 3,
-    numReviews: 12,
+  "name": "61 Keys Keyboard Electric Piano for Kids",
+  "image": "/uploads\\image-1695794216625.jpg",
+  "brand": "JAMGEL",
+  "category": "Electronics",
+  "description": "Unleash your child's musical talent with the 61 Keys Keyboard Electric Piano. Complete with a built-in microphone, it's perfect for budding musicians. Easy to use, it ensures endless musical fun for kids.",
+  "reviews": [
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2ab"
+      },
+      "name": "Amy Lee",
+      "rating": 5,
+      "comment": "My daughter loves it! The sound quality is great and the built-in microphone is a fun addition. Highly recommend for young music enthusiasts.",
+      "reviewTime": {
+        "$date": "2023-05-28T05:22:06.111Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c901c"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:54.998Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:54.998Z"
+      }
+    }
+  ],
+  "rating": 5,
+  "numReviews": 1,
+  "price": 89.99,
+  "countInStock": 10,
+  "__v": 0,
+  "createdAt": {
+    "$date": "2023-09-28T07:09:54.997Z"
   },
-  {
-    name: "Sony Playstation 5",
-    image: "/images/playstation.jpg",
-    description:
-      "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
-    brand: "Sony",
-    category: "Electronics",
-    price: 399.99,
-    countInStock: 11,
-    rating: 5,
-    numReviews: 12,
+  "updatedAt": {
+    "$date": "2023-09-28T07:09:54.997Z"
+  }
+},
+{
+  "_id": {
+    "$oid": "651526c28f374b07b16c901d"
   },
-  {
-    name: "Logitech G-Series Gaming Mouse",
-    image: "/images/mouse.jpg",
-    description:
-      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
-    brand: "Logitech",
-    category: "Electronics",
-    price: 49.99,
-    countInStock: 7,
-    rating: 3.5,
-    numReviews: 10,
+  "user": {
+    "$oid": "651526c28f374b07b16c9003"
   },
-  {
-    name: "Amazon Echo Dot 3rd Generation",
-    image: "/images/alexa.jpg",
-    description:
-      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
-    brand: "Amazon",
-    category: "Electronics",
-    price: 29.99,
-    countInStock: 0,
-    rating: 4,
-    numReviews: 12,
+  "name": "PS5 Stand and Cooling Station",
+  "image": "/uploads\\image-1695795135026.jpg",
+  "brand": "ElineKart",
+  "category": "Electronics",
+  "description": "Keep your PS5 cool and controllers charged with this multifunctional stand and cooling station. It's a sleek, space-saving solution ensuring your console and controllers are always ready for action. Review (2):",
+  "reviews": [
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2ac"
+      },
+      "name": "Jake Smith",
+      "rating": 5,
+      "comment": "A game-changer for my PS5 setup. Keeps everything organized, cool, and charged. A must-have for serious gamers.",
+      "reviewTime": {
+        "$date": "2023-06-10T04:12:05.101Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c901e"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.001Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.001Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2ad"
+      },
+      "name": "Mia Brown",
+      "rating": 4,
+      "comment": "Efficient and sleek design. It fits perfectly and does its job well. Very satisfied with this purchase.",
+      "reviewTime": {
+        "$date": "2023-06-15T03:14:02.091Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c901f"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.002Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.002Z"
+      }
+    }
+  ],
+  "rating": 4.5,
+  "numReviews": 2,
+  "price": 49.99,
+  "countInStock": 7,
+  "__v": 0,
+  "createdAt": {
+    "$date": "2023-09-28T07:09:55.001Z"
   },
-];
+  "updatedAt": {
+    "$date": "2023-09-28T07:09:55.001Z"
+  }
+},
+{
+  "_id": {
+    "$oid": "651526c28f374b07b16c9020"
+  },
+  "user": {
+    "$oid": "651526c28f374b07b16c9003"
+  },
+  "name": "Bengoo G9000 Stereo Gaming Headset",
+  "image": "/uploads\\image-1695794481803.webp",
+  "brand": "Bengoo",
+  "category": "Electronics",
+  "description": "Experience superior sound quality with the Bengo G9000 Stereo Gaming Headset. Perfect for various consoles, it offers noise-canceling and a comfortable over-ear design, enhancing your gaming experience.",
+  "reviews": [],
+  "rating": 0,
+  "numReviews": 0,
+  "price": 22.99,
+  "countInStock": 5,
+  "__v": 0,
+  "createdAt": {
+    "$date": "2023-09-28T07:09:55.003Z"
+  },
+  "updatedAt": {
+    "$date": "2023-09-28T07:09:55.003Z"
+  }
+},
+{
+  "_id": {
+    "$oid": "651526c28f374b07b16c9021"
+  },
+  "user": {
+    "$oid": "651526c28f374b07b16c9003"
+  },
+  "name": "HD 1080p Webcam",
+  "image": "/uploads\\image-1695795117748.webp",
+  "brand": "VISSPL",
+  "category": "Electronics",
+  "description": "Enhance your online interactions with this HD 1080p Webcam. Offering clear video and sound with a wide view, it’s perfect for streaming, conferencing, and gaming.",
+  "reviews": [
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2ae"
+      },
+      "name": "Oliver Johnson",
+      "rating": 5,
+      "comment": "Clear video and sound quality. It's perfect for my online meetings and streaming needs.",
+      "reviewTime": {
+        "$date": "2023-07-01T02:22:01.111Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9022"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.005Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.005Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2af"
+      },
+      "name": "Amelia Smith",
+      "rating": 5,
+      "comment": "Affordable and efficient. It was easy to set up and works seamlessly.",
+      "reviewTime": {
+        "$date": "2023-07-05T05:12:03.121Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9023"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.006Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.006Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b0"
+      },
+      "name": "Henry Taylor",
+      "rating": 5,
+      "comment": "Impressed with the widescreen view and clarity. A great buy for the price.",
+      "reviewTime": {
+        "$date": "2023-07-10T06:14:04.131Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9024"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.006Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.006Z"
+      }
+    }
+  ],
+  "rating": 5,
+  "numReviews": 3,
+  "price": 33.99,
+  "countInStock": 11,
+  "__v": 0,
+  "createdAt": {
+    "$date": "2023-09-28T07:09:55.003Z"
+  },
+  "updatedAt": {
+    "$date": "2023-09-28T07:09:55.003Z"
+  }
+},
+{
+  "_id": {
+    "$oid": "651526c28f374b07b16c9025"
+  },
+  "user": {
+    "$oid": "651526c28f374b07b16c9003"
+  },
+  "name": "Instant Pot Duo 7-in-1 Electric Pressure Cooker",
+  "image": "/uploads\\image-1695794831421.webp",
+  "brand": "Instant Pot",
+  "category": "Electronics",
+  "description": "Make cooking effortless with the Instant Pot Duo 7-in-1 Electric Pressure Cooker. From sautéing to steaming, enjoy various features and an app with over 800 recipes. A kitchen must-have!",
+  "reviews": [
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b1"
+      },
+      "name": "Oliver Johnson",
+      "rating": 5,
+      "comment": "A lifesaver in the kitchen. So versatile and easy to use. The app recipes are a bonus!",
+      "reviewTime": {
+        "$date": "2023-06-01T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9026"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.008Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.008Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b2"
+      },
+      "name": "Emma Patel",
+      "rating": 4,
+      "comment": "Durable and efficient. It has made cooking so much more convenient.",
+      "reviewTime": {
+        "$date": "2023-06-10T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9027"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.009Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.009Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b3"
+      },
+      "name": "Aiden Smith",
+      "rating": 3,
+      "comment": "Love the multiple functions. It’s a high-quality appliance worth the investment.",
+      "reviewTime": {
+        "$date": "2023-06-23T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9028"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.010Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.010Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b4"
+      },
+      "name": "Ella Kim",
+      "rating": 4,
+      "comment": "Perfect for my busy schedule. Quick, convenient, and easy to clean.",
+      "reviewTime": {
+        "$date": "2023-07-14T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9029"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.010Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.010Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b5"
+      },
+      "name": "James Lee",
+      "rating": 4,
+      "comment": "Amazing pressure cooker. The recipes app is very helpful and the food turns out great.",
+      "reviewTime": {
+        "$date": "2023-08-19T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c902a"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.011Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.011Z"
+      }
+    }
+  ],
+  "rating": 4,
+  "numReviews": 5,
+  "price": 29.99,
+  "countInStock": 4,
+  "__v": 0,
+  "createdAt": {
+    "$date": "2023-09-28T07:09:55.007Z"
+  },
+  "updatedAt": {
+    "$date": "2023-09-28T07:09:55.007Z"
+  }
+},
+{
+  "_id": {
+    "$oid": "651526c28f374b07b16c902b"
+  },
+  "user": {
+    "$oid": "651526c28f374b07b16c9003"
+  },
+  "name": "10.1 Inch WiFi Digital Photo Frame",
+  "image": "/uploads\\image-1695794725093.webp",
+  "brand": "Anna Bella",
+  "category": "Electronics",
+  "description": "Display cherished memories with the 10.1 Inch WiFi Digital Photo Frame. With IPS HD touch screen and 16GB storage, share photos or videos remotely and enjoy the auto-rotate feature.",
+  "reviews": [
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b6"
+      },
+      "name": "Charlotte Choi",
+      "rating": 5,
+      "comment": "Love the touch screen and storage space. It’s easy to share photos and the display is beautiful.",
+      "reviewTime": {
+        "$date": "2023-05-12T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c902c"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.013Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.013Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b7"
+      },
+      "name": "Mia Taylor",
+      "rating": 5,
+      "comment": "A sleek and modern frame that works flawlessly. The auto-rotate feature is a nice touch.",
+      "reviewTime": {
+        "$date": "2023-07-16T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c902d"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.021Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.021Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b8"
+      },
+      "name": "Ella Brown",
+      "rating": 5,
+      "comment": "Great quality and easy to set up. It makes a perfect gift.",
+      "reviewTime": {
+        "$date": "2023-07-29T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c902e"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.021Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.021Z"
+      }
+    }
+  ],
+  "rating": 5,
+  "numReviews": 3,
+  "price": 89.99,
+  "countInStock": 9,
+  "__v": 0,
+  "createdAt": {
+    "$date": "2023-09-28T07:09:55.013Z"
+  },
+  "updatedAt": {
+    "$date": "2023-09-28T07:09:55.013Z"
+  }
+},
+{
+  "_id": {
+    "$oid": "651526c28f374b07b16c902f"
+  },
+  "user": {
+    "$oid": "651526c28f374b07b16c9003"
+  },
+  "name": "Lefant Robot Vacuum Cleaner",
+  "image": "/uploads\\image-1695794889743.webp",
+  "brand": "Lefant",
+  "category": "Electronics",
+  "description": "Enjoy effortless cleaning with the Lefant Robot Vacuum Cleaner. With powerful suction and multiple cleaning modes, it’s ideal for pet hair and hard floors, ensuring a spotless home.",
+  "reviews": [
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2b9"
+      },
+      "name": "James Kim",
+      "rating": 5,
+      "comment": "It’s a game-changer for pet owners. Powerful suction and it navigates easily.",
+      "reviewTime": {
+        "$date": "2023-05-15T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9030"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.023Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.023Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2ba"
+      },
+      "name": "Charlotte Lee",
+      "rating": 4,
+      "comment": "Love the self-charging feature. It keeps my home clean without any effort.",
+      "reviewTime": {
+        "$date": "2023-06-26T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9031"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.024Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.024Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2bb"
+      },
+      "name": "Benjamin Choi",
+      "rating": 4,
+      "comment": "Efficient and quiet. It works great on hard floors and easily picks up pet hair.",
+      "reviewTime": {
+        "$date": "2023-07-02T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9032"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.024Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.024Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2bc"
+      },
+      "name": "Amelia Patel",
+      "rating": 4,
+      "comment": "A reliable robot vacuum with great features. Worth the investment.",
+      "reviewTime": {
+        "$date": "2023-07-28T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9033"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.024Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.024Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2bd"
+      },
+      "name": "Henry Smith",
+      "rating": 5,
+      "comment": "Impressive cleaning and easy to control with the app. Highly recommend.",
+      "reviewTime": {
+        "$date": "2023-08-13T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9034"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.024Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.024Z"
+      }
+    },
+    {
+      "user": {
+        "$oid": "65150ce4a5d7374f3be0d2be"
+      },
+      "name": "Ella Kim",
+      "rating": 3,
+      "comment": "Fantastic vacuum cleaner. It has made my life so much easier.",
+      "reviewTime": {
+        "$date": "2023-08-25T07:22:05.141Z"
+      },
+      "_id": {
+        "$oid": "651526c28f374b07b16c9035"
+      },
+      "createdAt": {
+        "$date": "2023-09-28T07:09:55.025Z"
+      },
+      "updatedAt": {
+        "$date": "2023-09-28T07:09:55.025Z"
+      }
+    }
+  ],
+  "rating": 4,
+  "numReviews": 6,
+  "price": 219.99,
+  "countInStock": 7,
+  "__v": 0,
+  "createdAt": {
+    "$date": "2023-09-28T07:09:55.023Z"
+  },
+  "updatedAt": {
+    "$date": "2023-09-28T07:09:55.023Z"
+  }
+},
+{
+  "_id": {
+    "$oid": "651526c28f374b07b16c9036"
+  },
+  "user": {
+    "$oid": "651526c28f374b07b16c9003"
+  },
+  "name": "VISSPL Full HD Mini Projector",
+  "image": "/uploads\\image-1695794986137.webp",
+  "brand": "VISSPL",
+  "category": "Electronics",
+  "description": "Bring the cinema home with the VISSPL Full HD Mini Projector. Portable and compatible with multiple devices, enjoy movies, games, and more in high definition anywhere.",
+  "reviews": [],
+  "rating": 0,
+  "numReviews": 0,
+  "price": 79.99,
+  "countInStock": 5,
+  "__v": 0,
+  "createdAt": {
+    "$date": "2023-09-28T07:09:55.026Z"
+  },
+  "updatedAt": {
+    "$date": "2023-09-28T07:09:55.026Z"
+  }
+}];
 
 export default products;
