78b5a5d789520de82e75f0be8e997ba48e692fa5
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-20 12:43:25 +0530

    added the functionality to add product to cart in the ProductTab component

--- src/components/ProductTab.jsx
+++ src/components/ProductTab.jsx
@@ -1,6 +1,8 @@
 import { BASE_URL } from '../constants'
 import { useNavigate } from 'react-router-dom'
 import getStars from '../utils/getStars'
+import { addToCart } from "../slices/cartSlice";
+import { useDispatch } from "react-redux";
 
 const ProductTab = ({ product }) => {
 
@@ -10,20 +12,26 @@ const ProductTab = ({ product }) => {
     rating, 
     numReviews, 
     price,
+    countInStock,
     _id
   } = product
 
-  const navigation = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const stars = getStars(rating);
   
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty: 1 }));
+    navigate("/cart");
+  };
   
   return (
-    <div className="product_tab" onClick={() => navigation(`product/${_id}`)}>
-      <img className="product_img" src={`${BASE_URL}${image}`} alt={name} />
+    <div className="product_tab">
+      <img className="product_img" src={`${BASE_URL}${image}`} alt={name} onClick={() => navigate(`product/${_id}`)} />
       <div className='product_tab_flex'>
-        <h3 className="product_title">{name}</h3>
-        <button className='add_to_cart_btn'>+</button>
+        <h3 className="product_title" onClick={() => navigate(`product/${_id}`)}>{name}</h3>
+        <button className='add_to_cart_btn' disabled={product.countInStock === 0} onClick={addToCartHandler}>+</button>
       </div>
       <div className='star_rating'>
         {stars} <p>{numReviews}</p>
