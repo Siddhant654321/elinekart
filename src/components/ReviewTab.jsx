6ac6b74a1e986591b386f5d5f27c035085e4b1ec
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-20 12:29:21 +0530

    created and styled rest of the ReviewTab section

--- src/components/ReviewTab.jsx
+++ src/components/ReviewTab.jsx
@@ -5,9 +5,10 @@ const ReviewTab = ({ comment, name, rating, createdAt }) => {
   const stars = getStars(rating);
   return (
     <div className="review_tab">
-      <p>{comment}</p>
+      <p className="review_comment">{comment}</p>
       <h3>{name}</h3>
       <div className="review_tab_rating_container">{stars}</div>
+      <p>{createdAt.split('T')[0]}</p>
     </div>
   );
 };
