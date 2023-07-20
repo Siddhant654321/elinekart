4f61e4eac65dce88551a615ac19b82d6d9c7ba7c
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-20 12:17:33 +0530

    added box shadow and rating to ReviewTab component

--- src/components/ReviewTab.jsx
+++ src/components/ReviewTab.jsx
@@ -1,11 +1,14 @@
-import './ReviewTab.css';
+import getStars from "../utils/getStars";
+import "./ReviewTab.css";
 
-const ReviewTab = ({ comment, name, rating, createdAt}) => {
+const ReviewTab = ({ comment, name, rating, createdAt }) => {
+  const stars = getStars(rating);
   return (
     <div className="review_tab">
-        <p>{comment}</p>
-        <h3>{name}</h3>
+      <p>{comment}</p>
+      <h3>{name}</h3>
+      <div className="review_tab_rating_container">{stars}</div>
     </div>
-  )
-}
-export default ReviewTab
+  );
+};
+export default ReviewTab;
