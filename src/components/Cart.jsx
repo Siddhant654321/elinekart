a1a76aa284d38fb7f354a7eca7d26744bea74ed9
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-21 16:02:30 +0530

    created the header of the cart page and styled it

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,8 +2,15 @@ import './Cart.css'
 
 const Cart = () => {
   return (
-    <div>
-
+    <div className='cart_page'>
+        <section>
+            <ul className='cart_headers'>
+                <li>PRODUCT</li>
+                <li>PRICE</li>
+                <li>QUANTITY</li>
+                <li>REMOVE</li>
+            </ul>
+        </section>
     </div>
   )
 }
