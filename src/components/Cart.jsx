7547e2c41d7eb423dde439393ad6b7bc71639ad8
Siddhant Singh <technosys.sidd@gmail.com>
2023-07-22 11:44:04 +0530

    created the bottom section of the cart page and changed a few styles in the existing cart page code to make it look better

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,6 +6,7 @@ import trash from '../assets/trash.svg'
 const Cart = () => {
 
   const cartItems = useSelector((state) => state?.cart?.cartItems);
+  let total = 0;
 
   return (
     <div className="cart_page">
@@ -25,10 +26,26 @@ const Cart = () => {
             <p className="cart_product_price">${product.price}</p>
             <p className="cart_product_qty">{product.qty}</p>
             <div>
-                <img src={trash} alt="Remove" />
+                <img src={trash} alt="Remove" width={32} height={32} />
             </div>
           </div>;
         })}
+      </section>
+      <section>
+        <ul className="cart_headers">
+          <li>ORDER SUMMARY</li>
+        </ul>
+        {cartItems.map((product) => {
+          total += product.price
+          return <div key={product._id} className="cart_summary_product">
+            <p>{product.name}</p>
+            <p>${product.price}</p>
+          </div>
+        })}
+        <div className="cart_total_container">
+          <p>TOTAL</p>
+          <p className="total_price">${total}</p>
+        </div>
       </section>
     </div>
   );
