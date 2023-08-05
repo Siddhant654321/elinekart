4b21aa0cd84ff05b5cb14ceaed4685be63e58bc7
Siddhant Singh <technosys.sidd@gmail.com>
2023-08-05 21:59:43 +0530

    fixed logout not working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,7 +59,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.cookie("jwt", "", {
+  res.cookie("token", "", {
     httpOnly: true,
     expires: new Date(0),
   });
@@ -188,5 +188,5 @@ export {
   getUsers,
   getUserByID,
   deleteUser,
-  updateUser
+  updateUser,
 };
