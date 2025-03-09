19d48cc483a045ee04f952c0703e97cbd3ca9a7d
Siddhant Singh <technosys.sidd@gmail.com>
2025-03-09 11:15:55 +0530

    edited the navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,14 @@
-import './Navbar.css';
-import search_icon from '../assets/search_icon.svg'; 
-import close_icon from '../assets/close_icon.svg';
-import hamburger_icon from '../assets/hamburger_icon.svg';
-import { useState } from 'react';
-import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useLogoutMutation } from '../slices/usersApiSlice';
-import { logout } from '../slices/authSlice';
+import "./Navbar.css";
+import { useState } from "react";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 
 const Navbar = () => {
-
   const { keyword: urlKeyword } = useParams();
 
-  const [keyword, setKeyword] = useState(urlKeyword || "");
-
-  const [showMenu, setShowMenu] = useState(() => false)
+  const [showMenu, setShowMenu] = useState(false);
 
   const { userInfo } = useSelector((state) => state.auth);
 
@@ -33,16 +27,6 @@ const Navbar = () => {
     }
   };
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    if (keyword) {
-      navigate(`/products/search/${keyword.trim()}`);
-      setKeyword("");
-    } else {
-      navigate("/");
-    }
-  };
-
   return (
     <>
       <div className="navbar_container">
@@ -51,91 +35,95 @@ const Navbar = () => {
             ELINEKART
           </Link>
         </div>
-        {!showMenu ? (
-          <img
-            src={hamburger_icon}
-            className="hamburger_icon"
-            onClick={() => setShowMenu(() => true)}
-          />
-        ) : (
-          <img
-            src={close_icon}
-            className="close_icon"
-            onClick={() => setShowMenu(() => false)}
-          />
-        )}
-        <form className={`navbar_search_container ${showMenu && "show_menu"}`} onSubmit={submitHandler}>
-          <input
-            type="text"
-            placeholder="Search Products"
-            className="navbar_search_input"
-            onChange={(e) => setKeyword(e.target.value)}
-            value={keyword}
-          />
-          <button type="submit" className="navbar_search_button">
-            <img src={search_icon} height={34} width={34} />
-          </button>
-        </form>
-        <div className={`navbar_links_container ${showMenu && "show_menu"}`}>
-          <Link className="navbar_links" to="/products">
+
+        <button
+          className={`menu_toggle ${showMenu ? "open" : ""}`}
+          onClick={() => setShowMenu(!showMenu)}
+          aria-label="Toggle menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <div
+          className={`navbar_links_container ${showMenu ? "show_menu" : ""}`}
+        >
+          <Link
+            className="navbar_links"
+            to="/products"
+            onClick={() => setShowMenu(false)}
+          >
             PRODUCTS
           </Link>
-          <Link className="navbar_links" to="/cart">
+          <Link
+            className="navbar_links"
+            to="/cart"
+            onClick={() => setShowMenu(false)}
+          >
             CART
+          </Link>
+          <Link
+            className="navbar_links"
+            to="/products"
+            onClick={() => setShowMenu(false)}
+          >
+            CONTACT US
           </Link>
           {userInfo ? (
             <>
-              <Link className="navbar_links" to="/profile">
+              <Link
+                className="navbar_links"
+                to="/profile"
+                onClick={() => setShowMenu(false)}
+              >
                 PROFILE
               </Link>
-              <div style={{ alignSelf: "center" }} onClick={logoutHandler}>
+              <div className="navbar_logout_wrapper" onClick={logoutHandler}>
                 <svg
-                  className='navbar_logout_icon'
+                  className="navbar_logout_icon"
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <g id="SVGRepo_bgCarrier" stroke-width="0" />
-
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                  <path
+                    d="M21 12L13 12"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
-
-                  <g id="SVGRepo_iconCarrier">
-                    {" "}
-                    <path
-                      d="M21 12L13 12"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />{" "}
-                    <path
-                      d="M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />{" "}
-                    <path
-                      d="M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />{" "}
-                  </g>
+                  <path
+                    d="M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
                 </svg>
               </div>
             </>
           ) : (
             <>
-              <Link className="navbar_links" to="/signin">
+              <Link
+                className="navbar_links"
+                to="/signin"
+                onClick={() => setShowMenu(false)}
+              >
                 SIGN IN
               </Link>
-              <Link className="navbar_links" to="/register">
+              <Link
+                className="navbar_links"
+                to="/register"
+                onClick={() => setShowMenu(false)}
+              >
                 REGISTER
               </Link>
             </>
@@ -145,5 +133,6 @@ const Navbar = () => {
       <Outlet />
     </>
   );
-}
-export default Navbar
+};
+
+export default Navbar;
