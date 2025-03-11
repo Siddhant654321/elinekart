6e592f9cdfbc8bc04f1600f8679e2b9f40453df3
Siddhant Singh <technosys.sidd@gmail.com>
2025-03-11 20:53:38 +0530

    modified the hero section of the website with a new and better design

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,26 +1,35 @@
 import "./HeroSection.css";
-import hero_image from "../assets/hero_image.png";
-import arrow_icon from "../assets/arrow_icon.svg";
 import { Link } from "react-router-dom";
+import left_hs from "../assets/left_hs.png";
+import right_hs from "../assets/right_hs.png";
+import top_hs from "../assets/top_hs.png";
+import bottom_hs from "../assets/bottom_hs.png";
 
 const HeroSection = () => {
   return (
     <section className="section_container">
-      <div className="left_side">
-        <h1>
-          WE OFFER YOU THE <span>BEST </span> COLLECTION OF <span>GADGETS</span>
-        </h1>
-        <p>Your ultimate destination for the finest electronic gadgets</p>
-        <Link className="explore_btn_link_tag" to="/products">
-          <button className="explore_btn">
-            Explore the collection <img src={arrow_icon} alt="" />
-          </button>
-        </Link>
+      <div className="left_section">
+        <img src={left_hs} className="left_hs" />
       </div>
-      <div className="right_side">
-        <img src={hero_image} alt="" />
+      <div className="middle_section">
+        <div>
+          <img src={top_hs} className="top_hs" />
+        </div>
+        <div className="center_container">
+          <h2 className="hs_first_heading">PREMIUM</h2>
+          <h2 className="hs_second_heading">COLLECTION</h2>
+          <Link to="/products">
+            <button className="white_btn">SHOP NOW</button>
+          </Link>
+        </div>
+        <div>
+          <img src={bottom_hs} className="bottom_hs" />
+        </div>
+      </div>
+      <div className="right_section">
+        <img src={right_hs} className="right_hs" />
       </div>
     </section>
-  )
-}
-export default HeroSection
+  );
+};
+export default HeroSection;
